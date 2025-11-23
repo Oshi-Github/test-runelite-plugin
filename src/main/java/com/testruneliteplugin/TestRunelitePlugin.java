824ecad38851d4d1319c048ff4cf0d4dc098c6d3
oshi <oshigithub@gmail.com>
2025-11-23 19:07:17 +0000

    Rename plugin name in plugin descriptor

--- src/main/java/com/testruneliteplugin/TestRunelitePlugin.java
+++ src/main/java/com/testruneliteplugin/TestRunelitePlugin.java
@@ -14,7 +14,7 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Test RuneLite Plugin"
 )
 public class TestRunelitePlugin extends Plugin
 {
